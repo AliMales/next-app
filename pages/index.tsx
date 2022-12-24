1c6128260b9544c9a72bd530547ed8ff4eda4827
tigerzh <only_tigerhu@163.com>
2022-12-24 10:13:22 +0800

    feat:redict doc domain

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 console.log(inter)
 
 export default function Home() {
+  window.location.href='https://doc.tigerzh.com'
   return (
     <>
       <Head>
